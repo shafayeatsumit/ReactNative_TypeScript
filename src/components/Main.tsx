ff515a4500550c6987013686811bbbd44d5de20d
shafayeat sumit <shafayeatkabir@gmail.com>
2020-12-15 15:14:59 +0600

    build minimal ui for TODO

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { Todo, fetchTodos } from '../actions';
 import { AppState } from '../reducers';
@@ -10,11 +16,48 @@ interface MainProps {
   fetchTodos(): any;
 }
 
+interface Item {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 class _Main extends Component<MainProps> {
+  handleXout = (): void => { };
+
+  renderItem = ({ item }: { item: Todo }) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.todoText}>{item.title}</Text>
+        <TouchableOpacity onPress={this.handleXout} style={styles.xout}>
+          <Text style={styles.xoutText}>X</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  renderList = (): JSX.Element => {
+    return (
+      <View style={{ marginTop: 150 }}>
+        <FlatList
+          data={this.props.todos}
+          renderItem={this.renderItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+    );
+  };
+
+  fetchTodos = (): void => {
+    this.props.fetchTodos();
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <TouchableOpacity style={styles.fetchButton} onPress={this.fetchTodos}>
+          <Text style={styles.buttonTitle}>Fetch</Text>
+        </TouchableOpacity>
+        {this.renderList()}
       </View>
     );
   }
@@ -37,5 +80,55 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#B8BAC6',
+  },
+  todo: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  fetchButton: {
+    position: 'absolute',
+    top: 50,
+    backgroundColor: 'tomato',
+    height: 70,
+    width: 150,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTitle: {
+    color: 'white',
+    fontSize: 30,
+  },
+  item: {
+    height: 60,
+    marginVertical: 10,
+    width: 380,
+    borderRadius: 10,
+    // justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    // paddingLeft: 35,
+    paddingLeft: 10,
+    paddingRight: 40,
+
+    flexDirection: 'row',
+    backgroundColor: '#E1E2E7',
+  },
+  todoText: {
+    color: 'gray',
+    fontSize: 18,
+    textAlign: 'left',
+  },
+  xoutText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#4A63A9',
+  },
+  xout: {
+    position: 'absolute',
+    right: 15,
+    alignSelf: 'center',
+    paddingLeft: 10,
   },
 });
