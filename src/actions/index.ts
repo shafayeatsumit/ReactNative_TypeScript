c6f9d33c3ce5806b0094a643ad1336f2f03d097e
shafayeat sumit <shafayeatkabir@gmail.com>
2020-12-15 12:24:43 +0600

    action creators and action types setup

---
+++ src/actions/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+    dispatch({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
+  };
+};
---
+++ src/actions/types.ts
@@ -0,0 +1,5 @@
+// these action types has to be unique
+// it doesn't require to assign any string.
+export enum ActionTypes {
+  fetchTodos,
+}
